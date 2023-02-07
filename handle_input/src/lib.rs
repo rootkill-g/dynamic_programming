3e1ed5f127691a51931cefc7c5e8d0afa4abe7d0
rootkill-g <25330922+rootkill-g@users.noreply.github.com>
2023-02-08 00:28:48 +0530

    Created a Local Crate to handle inputs, and Added Documentation

---
+++ handle_input/src/lib.rs
@@ -0,0 +1,46 @@
+// Using the standard library's io and str modules
+use std::{
+    io::prelude::*,
+    str,
+};
+
+// Definig the Scanner struct
+pub struct Scanner<R> {
+    // Reader
+    pub reader: R,
+    // Buffered String
+    pub bufstr: Vec<u8>,
+    // Buffered Iterator
+    pub bufiter: str::SplitWhitespace<'static>,
+}
+
+// Implementing for Scanner Struct
+impl<R: BufRead> Scanner<R> {
+    // `new` function to initialize a new Scanner
+    pub fn new(reader: R) -> Self {
+        Self {
+            reader, 
+            bufstr: vec![],
+            bufiter: "".split_whitespace(),
+        }
+    }
+
+    // `input` handles the input
+    pub fn input<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(input) = self.bufiter.next() {
+                return input.parse().ok().expect("Failed to parse input");
+            }
+
+            self.bufstr.clear();
+
+            self.reader.read_until(b'\n', &mut self.bufstr).expect("Failed to read input");
+
+            self.bufiter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.bufstr);
+
+                std::mem::transmute(slice.split_whitespace())
+            }
+        }
+    }
+}
--- recursive_fun/src/main.rs
+++ recursive_fun/src/main.rs
@@ -1,64 +1,42 @@
-use std::{
-    io::{self, prelude::*},
-    str,
-};
-
-struct Scanner<R> {
-    reader: R,
-    bufstr: Vec<u8>,
-    bufiter: str::SplitWhitespace<'static>,
-}
-
-impl<R: BufRead> Scanner<R> {
-    fn new(reader: R) -> Self {
-        Self {
-            reader, 
-            bufstr: vec![],
-            bufiter: "".split_whitespace(),
-        }
-    }
-
-    fn input<T: str::FromStr>(&mut self) -> T {
-        loop {
-            if let Some(input) = self.bufiter.next() {
-                return input.parse().ok().expect("Failed to parse input");
-            }
-
-            self.bufstr.clear();
-
-            self.reader.read_until(b'\n', &mut self.bufstr).expect("Failed to read input");
-
-            self.bufiter = unsafe {
-                let slice = str::from_utf8_unchecked(&self.bufstr);
-
-                std::mem::transmute(slice.split_whitespace())
-            }
-        }
-    }
-}
+// Using the local crate for handling inputs
+use handle_input::*;
+// Using the standard input library
+use std::io::{self, prelude::*};
 
 fn main() {
+    // Define the input and output streams
     let (stdin, stdout) = (io::stdin(), io::stdout());
 
+    // Defining the `in` Scanner from `handle_input` crate
     let mut scan = Scanner::new(stdin.lock());
 
+    // Defining the `out` buffer sink
     let mut out = io::BufWriter::new(stdout.lock());
 
+    // Write to the `out` sink
     write!(out, "Enter a number to recursive add all smaller numbers to it: ").ok();
 
+    // The `write!` and `writeln!` macros need to be flushed 
+    // to put output on the defined sink
     out.flush().unwrap();
 
+    // Take the number input
     let num = scan.input::<usize>();
 
+    // res will hold the sum of all the numbers from the value `num` to `1`
     let res = recursive_sum(num);
 
+    // Write the result of recursive sum to the output stream (sink)
     writeln!(out, "Recursive Sum is : {res}").ok();
 }
 
+// Defining the function to add numbers recursively
 fn recursive_sum(n: usize) -> usize {
+    // Self Explanatory
     if n == 0 {
         return n
     }
 
+    // If `n != 0`, then we call the function again with `n - 1`
     n + recursive_sum(n - 1)
 }
