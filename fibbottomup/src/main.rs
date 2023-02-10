842ce0bf1f005fa520c9f433f053423ebad8ab17
rootkill-g <25330922+rootkill-g@users.noreply.github.com>
2023-02-10 22:40:15 +0530

    Mistakenly implemented bottom up in top down approach, is corrected

---
+++ fibbottomup/src/main.rs
@@ -0,0 +1,28 @@
+use handle_input::*;
+use std::io::{self, prelude::*};
+
+fn main() {
+    let (stdin, stdout) = (io::stdin(), io::stdout());
+    let mut scan = Scanner::new(stdin.lock());
+    let mut out = io::BufWriter::new(stdout.lock());
+    let n: u128 = scan.input();
+    let res = fibbottomup(n);
+    write!(out, "{res}").ok();
+    out.flush().unwrap();
+}
+
+fn fibbottomup(n: u128) -> u128 {
+    if n < 2 {
+        return n;
+    }
+    let mut fib_cache = vec![0; (n + 1) as usize];
+
+    fib_cache[0] = 0;
+    fib_cache[1] = 1;
+
+    for i in 2..(n + 1) {
+        fib_cache[i as usize] = fib_cache[(i - 1) as usize] + fib_cache[(i - 2) as usize];
+    }
+
+    fib_cache[n as usize]
+}
--- fibtopdown/src/main.rs
+++ fibtopdown/src/main.rs
@@ -1,5 +1,5 @@
 use handle_input::*;
-use std::io::{prelude::*, self};
+use std::{collections::HashMap, io::{prelude::*, self}};
 
 fn main() {
     let (stdin, stdout) = (io::stdin(), io::stdout());
@@ -14,24 +14,28 @@ fn main() {
 
     let num = scan.input::<u128>();
 
-    let res = fibtopdown(num);
+    let mut cache: HashMap<u128, u128> = HashMap::new();
+
+    cache.insert(1, 1);
+    cache.insert(2, 1);
+
+    let res = fibtopdown(num, &mut cache);
 
     writeln!(out, "The number in fibonacci series at position : {num} is :\n=> {res}").ok();
 }
 
-fn fibtopdown(num: u128) -> u128 {
-    if num < 2 {
+fn fibtopdown(num: u128, cache: &mut HashMap<u128, u128>) -> u128 {
+    if num == 0 || num == 1 {
         return num;
     }
 
-    let mut fib_cache = vec![0; (num + 1) as usize];
-
-    fib_cache[0] = 0;
-    fib_cache[1] = 1;
-
-    for i in 2..(num + 1) {
-        fib_cache[i as usize] = fib_cache[(i - 1) as usize] + fib_cache[(i - 2) as usize];
+    if let Some(v) = cache.get(&num) {
+        return *v
     }
 
-    fib_cache[num as usize]
+    let res = fibtopdown(num - 1, cache) + fibtopdown(num - 2, cache);
+
+    cache.insert(num, res);
+
+    return cache[&num]
 }
